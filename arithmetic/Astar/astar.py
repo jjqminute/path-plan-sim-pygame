ba0fa7fbeeedb32427882178f8aee02caed58ce5
fgwxy <1257003181@qq.com>
2024-05-29 20:45:23 +0800

    更改A星bug，不会再穿过障碍物了。

--- arithmetic/Astar/astar.py
+++ arithmetic/Astar/astar.py
@@ -5,29 +5,33 @@ import time
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout
 from .Node import point
 import pygame
+from shapely.geometry import Point, Polygon, LineString
+
+
+
 
 class astar:  # 核心部分，寻路类
     def __init__(self, mapdata):
         self.start = point(mapdata.start_point[0], mapdata.start_point[1])  # 储存此次搜索的开始点
         self.end = point(mapdata.end_point[0], mapdata.end_point[1])  # 储存此次搜索的目的点
-        # self.Map = mapdata.map  # 一个二维数组，为此次搜索的地图引用
         self.open = []  # 开放列表：储存即将被搜索的节点
         self.close = []  # 关闭列表：储存已经搜索过的节点
         self.result = []  # 当计算完成后，将最终得到的路径写入到此属性中
         self.count = 0  # 记录此次搜索所搜索过的节点数
         self.useTime = 0  # 记录此次搜索花费的时间--在此演示中无意义，因为process方法变成了一个逐步处理的生成器，统计时间无意义。
-        # 开始进行初始数据处理
         self.open.append(self.start)
         self.width = mapdata.width
         self.surface = mapdata.surface
         self.height = mapdata.height
         self.radius = mapdata.obs_radius
-        # 尺寸暂定为1
         self.cell_size = 10
-        self.obstacles = mapdata.obstacles[0]
+        self.obstacles = [Polygon(obstacle) for obstacle in mapdata.obstacles]
         self.mapdata = mapdata
         self.screen = mapdata.plan_surface
-    # 将结果保存至txt文本中
+        # 膨胀障碍物
+
+
+
 
     # 计算F值
     def cal_F(self, loc):
@@ -47,27 +51,31 @@ class astar:  # 核心部分，寻路类
                 t = i
         return t
 
+    def line_intersects_obstacles(self, p1, p2):
+        line = LineString([(p1.x, p1.y), (p2.x, p2.y)])
+        return any(line.intersects(ob) for ob in self.obstacles)
     def getAroundPoint(self, loc):  # 获取指定点周围所有可通行的点，并将其对应的移动消耗进行赋值。
-        # 上 下 左 右 斜 八个 方向
+        directions = [
+            (loc.x, loc.y + self.cell_size, 10),
+            (loc.x + self.cell_size, loc.y + self.cell_size, 14),
+            (loc.x + self.cell_size, loc.y, 10),
+            (loc.x + self.cell_size, loc.y - self.cell_size, 14),
+            (loc.x, loc.y - self.cell_size, 10),
+            (loc.x - self.cell_size, loc.y - self.cell_size, 14),
+            (loc.x - self.cell_size, loc.y, 10),
+            (loc.x - self.cell_size, loc.y + self.cell_size, 14)
+        ]
 
-        l = [(loc.x, loc.y + self.cell_size, 10), (loc.x + self.cell_size, loc.y + self.cell_size, 14),
-             (loc.x + self.cell_size, loc.y, 10), (loc.x + self.cell_size, loc.y - self.cell_size, 14),
-             (loc.x, loc.y - self.cell_size, 10), (loc.x - self.cell_size, loc.y - self.cell_size, 14),
-             (loc.x - self.cell_size, loc.y, 10), (loc.x - self.cell_size, loc.y + self.cell_size, 14)]
-        # 全列表倒序查询
-        for i in l[::-1]:
-            # i[0]是x值，i[1]是y值 检测是否过界
-            if i[0] < 0 or i[0] >= self.width or i[1] < 0 or i[1] >= self.height:
-                l.remove(i)
-        nl = []
-        for i in l:
-            if self.surface.get_at((i[0], i[1])) != (0, 0, 0):
-                #
-                #print(list((i[0], i[1])))
-                nt = point(i[0], i[1])
-                nt.cost = i[2]
-                nl.append(nt)
-        return nl
+        valid_points = []
+        for direction in directions:
+            x, y, cost = direction
+            if 0 <= x < self.width and 0 <= y < self.height:
+                p = Point(x, y)
+                if not any(ob.contains(p) for ob in self.obstacles) and not self.line_intersects_obstacles(loc,p):
+                    nt = point(x, y)
+                    nt.cost = cost
+                    valid_points.append(nt)
+        return valid_points
 
     def distance(self, AS, point2):
         return math.sqrt((AS[0] - point2[0]) ** 2 + (AS[1] - point2[1]) ** 2)
@@ -80,6 +88,9 @@ class astar:  # 核心部分，寻路类
                 return False
         return True
 
+    def is_point_in_obstacles(self, p):
+        point_obj = Point(p.x, p.y)
+        return any(ob.contains(point_obj) for ob in self.obstacles)
     # 此次判断的点周围的可通行点加入到open列表中，如此点已经在open列表中则对其进行判断，如果此次路径得到的F值较之之前的F值更小，
     # 则将其父节点更新为此次判断的点，同时更新F、G值。
     def addToOpen(self, l,
@@ -131,7 +142,7 @@ class astar:  # 核心部分，寻路类
                     pygame.draw.rect(self.screen, (150, 150, 150), (k.x, k.y, self.cell_size, self.cell_size), 0)
                     break
 
-    def process(self,plan_surface):  # 使用yield将process方法变成一个生成器，可以逐步的对搜索过程进行处理并返回关键数据
+    def process(self, plan_surface):  # 使用yield将process方法变成一个生成器，可以逐步的对搜索过程进行处理并返回关键数据
         start = time.time()
         print("起点为", self.start)
         print("障碍物", self.obstacles)
@@ -147,9 +158,9 @@ class astar:  # 核心部分，寻路类
                 self.addToOpen(aroundP, tar)  # 把aroundP加入到open列表中并更新F值以及设定父节点
                 self.open.remove(tar)  # 将tar从open列表中移除
                 self.close.append(tar)  # 已经迭代过的节点tar放入close列表中
-                if self.end in self.open or self.getEstimate(tar)<15:  # 判断终点是否已经处于open列表中
+                if self.end in self.open or self.getEstimate(tar) < 15:  # 判断终点是否已经处于open列表中
                     e = self.end
-                    e.father=tar
+                    e.father = tar
                     self.result.append(e)
                     pygame.draw.circle(plan_surface, (0, 100, 255), (e.x, e.y), 2)
                     while True:
@@ -170,7 +181,7 @@ class astar:  # 核心部分，寻路类
         end = time.time()
         print("花费时间为")
         print(end - start)
-        return self.result,end-start
+        return self.result, end - start
         # self.repaint()
         # print('返回')
 
